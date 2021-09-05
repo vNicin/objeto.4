adca2faae0b1c3d61a0da3e0694f5d5ef509c3ef
vNicin <vinibarrosdalmeida@gmail.com>
2021-09-05 19:09:06 -0300

    Criação e posicionamento dos cubos no eixo y

---
+++ obj4/Assets/randColor.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randColor : MonoBehaviour
+{
+    struct Cube
+    {
+        public Vector3 position;
+        public Color color;
+    }
+
+    public ComputeShader computeShader;
+    public int iteractions = 50;
+    public int count = 100;
+    GameObject[] gameObjects;
+    Cube[] data;
+    public GameObject modelPref;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (data == null)
+        {
+
+
+            if (GUI.Button(new Rect(0, 0, 100, 50), "Create"))
+            {
+                createCube();
+            }
+        }
+
+        if(data!= null)
+        {
+            if(GUI.Button(new Rect(110,0,100,50), "Random CPU"))
+            {
+                for( int k = 0; k<iteractions; k++)
+                {
+
+                
+                    for ( int i = 0; i< gameObjects.Length; i++)
+                      {
+                        gameObjects[i].GetComponent<MeshRenderer>().material.SetColor("_Color", Random.ColorHSV());
+                     }
+                }
+            }
+        }
+
+        if(data != null)
+        {
+            if (GUI.Button(new Rect(220,0,100,50), "Random GPU"))
+            {
+                int totalSize = 4 * sizeof(float) + 3 * sizeof(float);
+                ComputeBuffer computeBuffer = new ComputeBuffer(data.Length, totalSize);//utilizando GPU
+                computeBuffer.SetData(data); //copiar dados da cpu pra gpu
+                computeShader.SetBuffer(0, "cubes", computeBuffer);
+                computeShader.SetInt("iteraction", iteractions);
+                computeShader.Dispatch(0, data.Length / 10, 1, 1);
+
+                computeBuffer.GetData(data); //passando os dados de volta para a cpu
+                for (int i = 0; i < gameObjects.Length; i++)
+                {
+                    gameObjects[i].GetComponent<MeshRenderer>().material.SetColor("_Color",data[i].color);
+                }
+
+                computeBuffer.Dispose();//libera a memoria q havia sido alocada
+
+            }
+        }
+    }
+    private void createCube()//ainda em cpu
+    {
+        data = new Cube[count * count];
+        gameObjects = new GameObject[count * count];
+
+        for (int i = 0; i < count; i++)
+        {
+            float offsetX = (-count / 2 + i);
+            for(int j =0; j<count; j++)
+            {
+                float offsetY = (-count / 2 + j);
+                Color _color = Random.ColorHSV();
+                GameObject go = GameObject.Instantiate(modelPref, new Vector3(offsetX * 0.7f, offsetY * 0.7f,0 ), Quaternion.identity);
+                go.GetComponent<MeshRenderer>().material.SetColor("_Color", _color);
+
+                gameObjects[i * count + j] = go;
+                data[i * count + j] = new Cube();
+                data[i * count + j].position = go.transform.position;
+                data[i * count + j].color = _color;
+            }
+        }
+    }
+
+
+}
---
+++ obj4/Assets/renderTexture.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class renderTexture : MonoBehaviour
+{
+    public ComputeShader computeShader;
+    public RenderTexture renderTexturer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderTexturer = new RenderTexture(256, 256, 32);
+        renderTexturer.enableRandomWrite = true;
+        renderTexturer.Create();
+
+        computeShader.SetTexture(0, "Result", renderTexturer);
+        computeShader.SetFloat("resolution", renderTexturer.width);
+        computeShader.Dispatch(0, renderTexturer.width / 8, renderTexturer.width/8, 1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
